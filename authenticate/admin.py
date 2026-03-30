8b9882666b04b9069b81c942984a9971c84e4a6e
Saleem-bazhil <ab588538@gmail.com>
2026-03-30 19:12:57 +0530

    update

--- authenticate/admin.py
+++ authenticate/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group, User
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "is_superuser")
+    search_fields = ("username", "email", "first_name", "last_name")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    list_filter_submit = True
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
