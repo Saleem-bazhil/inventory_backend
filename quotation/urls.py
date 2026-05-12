82bd78217b701ec8850da48e824ab71418d375d1
vaseem148 <mohamedvaseem114@gmail.com>
2026-05-12 10:18:52 +0530

    quatation section upgrades 12.5.26b

--- quotation/urls.py
+++ quotation/urls.py
@@ -5,10 +5,12 @@ from .views import (
     QuotationDetailView,
     QuotationListCreateView,
     SendQuotationView,
+    QuotationSummaryView,
 )
 
 urlpatterns = [
     path('quotations/', QuotationListCreateView.as_view(), name='quotation-list'),
+    path('quotations/summary/', QuotationSummaryView.as_view(), name='quotation-summary'),
     path('quotations/<int:pk>/', QuotationDetailView.as_view(), name='quotation-detail'),
     path('quotations/<int:pk>/send/', SendQuotationView.as_view(), name='quotation-send'),
     path('quotations/<int:pk>/customer-response/', CustomerResponseView.as_view(), name='quotation-customer-response'),
