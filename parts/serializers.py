670c2256a9b8c7d177f62a43191f76314c249592
vaseem148 <mohamedvaseem114@gmail.com>
2026-05-22 14:26:42 +0530

    part request section upgrades 22.5.26v

--- parts/serializers.py
+++ parts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 from .models import PartRequest
+from ticket.models import Ticket
+from ticket.serializers import TicketPartRequestImageSerializer
 
 
 class UserMiniSerializer(serializers.Serializer):
@@ -17,6 +19,46 @@ class UserMiniSerializer(serializers.Serializer):
         return profile.get_role_display() if profile else ''
 
 
+class PartRequestTicketSerializer(serializers.ModelSerializer):
+    part_request_images = TicketPartRequestImageSerializer(many=True, read_only=True)
+    service_type_display = serializers.CharField(source="get_service_type_display", read_only=True)
+    current_status_display = serializers.CharField(source="get_current_status_display", read_only=True)
+    region_display = serializers.CharField(source="get_region_display", read_only=True)
+
+    class Meta:
+        model = Ticket
+        fields = [
+            "id",
+            "ticket_number",
+            "form_number",
+            "work_order",
+            "cust_name",
+            "cust_contact",
+            "cust_email",
+            "cust_address",
+            "location",
+            "product_name",
+            "serial_number",
+            "model_number",
+            "brand",
+            "case_id",
+            "condition_received",
+            "service_type",
+            "service_type_display",
+            "priority",
+            "issue_description",
+            "current_status",
+            "current_status_display",
+            "region",
+            "region_display",
+            "cso_date",
+            "cso_image",
+            "part_request_image",
+            "part_request_images",
+            "created_at",
+        ]
+
+
 class PartRequestSerializer(serializers.ModelSerializer):
     requested_by = UserMiniSerializer(read_only=True)
     approved_by = UserMiniSerializer(read_only=True)
@@ -25,6 +67,7 @@ class PartRequestSerializer(serializers.ModelSerializer):
     region_display = serializers.CharField(source='ticket.get_region_display', read_only=True)
     urgency_display = serializers.CharField(source='get_urgency_display', read_only=True)
     status_display = serializers.CharField(source='get_status_display', read_only=True)
+    ticket_details = PartRequestTicketSerializer(source='ticket', read_only=True)
 
     class Meta:
         model = PartRequest
@@ -37,6 +80,7 @@ class PartRequestSerializer(serializers.ModelSerializer):
             'approved_by',
             'approved_at', 'rejection_reason',
             'received_at', 'created_at', 'updated_at',
+            'ticket_details',
         ]
         read_only_fields = [
             'id', 'requested_by', 'approved_by', 'approved_at',
