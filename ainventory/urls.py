1dedd2b5bfff76e255fcccaf02c0c0890de1f752
vaseem148 <mohamedvaseem114@gmail.com>
2026-05-22 14:26:18 +0530

    part request section upgrades 22.5.26v

--- ainventory/urls.py
+++ ainventory/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from authenticate.views import EngineersListView
 
@@ -18,3 +20,7 @@ urlpatterns = [
     path('api/auth/', include('authenticate.urls')),
     path('api/users/engineers/', EngineersListView.as_view(), name='engineers-list'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
