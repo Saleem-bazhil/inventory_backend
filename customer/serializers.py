4b7707956ae0e8a78d9ea135bbd2bb8500e9ebeb
vaseem148 <mohamedvaseem114@gmail.com>
2026-05-22 09:47:07 +0530

    customers sections upgrades 22.5.26v

--- customer/serializers.py
+++ customer/serializers.py
@@ -1,13 +1,18 @@
 from rest_framework import serializers
 
-from .models import Customer
+from ticket.models import Ticket
 
 
 class CustomerSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source="cust_name", allow_blank=True, required=False)
+    email = serializers.CharField(source="cust_email", allow_blank=True, required=False, allow_null=True)
+    phone = serializers.CharField(source="cust_contact", allow_blank=True, required=False, allow_null=True)
+    company = serializers.CharField(source="work_order", allow_blank=True, required=False)
     total_transactions = serializers.SerializerMethodField()
+    status = serializers.CharField(source="current_status", read_only=True)
 
     class Meta:
-        model = Customer
+        model = Ticket
         fields = [
             "id",
             "name",
@@ -16,8 +21,15 @@ class CustomerSerializer(serializers.ModelSerializer):
             "company",
             "total_transactions",
             "created_at",
+            "region",
+            "status",
+            "ticket_number",
+            "form_number",
         ]
         read_only_fields = ["id", "created_at"]
 
     def get_total_transactions(self, obj):
-        return 0
+        if obj.cust_contact:
+            return Ticket.objects.filter(cust_contact=obj.cust_contact).count()
+        return 1
+
